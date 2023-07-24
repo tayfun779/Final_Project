c814ed90100e50c38de45601dbc9ada69a6e11a6
Tayfun F. Nuri <tayfunfn@code.local>
2023-07-24 19:54:41 +0400

    Base entity class created

---
+++ Final project/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+namespace Final_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int option;
+
+
+            do
+            {
+                Console.WriteLine("1.To carry out operations on products");
+                Console.WriteLine("2. To carry out operations on sales");
+                Console.WriteLine("0 Exit from system");
+
+
+                Console.WriteLine("____________");
+                Console.WriteLine("Please enter an options");
+                Console.WriteLine("____________");
+
+                while (int.TryParse(Console.ReadLine(), out option))
+                {
+                    Console.WriteLine("Invalid option!");
+                    Console.WriteLine("Enter an option please!");
+                    Console.WriteLine("____________");
+                }
+                 
+               
+
+                switch (option)
+                {
+                    case 1:
+                        SubMenu.ProductSubMenu();
+                }
+            } while (option!=0);
+        }
+    }
+}
